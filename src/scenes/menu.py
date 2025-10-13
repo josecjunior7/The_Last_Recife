3ed07bf49bf7a88af4cc00d42efd9b5ec85a2103
José C. Júnior <lyojunior7@gmail.com>
2025-10-13 18:49:27 -0300

    added background in menu

--- src/scenes/menu.py
+++ src/scenes/menu.py
@@ -32,16 +32,38 @@ def menu(tela, clock):
         #desenhar_texto(tela, "O Ultimo Recife", fonte_titulo, BRANCO, LARGURA // 2, 80)
 
         # botão jogar
-        jogar_rect = pygame.Rect(500, 250, 200, 50)
-        pygame.draw.rect(tela, VERDE, jogar_rect)
+        jogar_rect = pygame.Rect(490, 522, 200, 50) # (1º Largura, 2º Altura)
+
+        # cria uma surface com transparência
+        jogar_surface = pygame.Surface((jogar_rect.width, jogar_rect.height), pygame.SRCALPHA)
+
+        # pinta a surface (último valor é alpha → 0=transparente, 255=opaco)
+        jogar_surface.fill((0, 255, 0, 0))  # semi-transparente
+
+        # desenha na tela
+        tela.blit(jogar_surface, jogar_rect.topleft)
+
+        # escreve o texto por cima
         desenhar_texto(tela, "Novo Jogo", fonte_opcoes, AZUL, jogar_rect.centerx, jogar_rect.y + 10)
 
+        # clique do mouse
         if jogar_rect.collidepoint(mouse) and clique[0]:
             return "jogar"
 
-        # botão sair
-        sair_rect = pygame.Rect(500, 310, 200, 50)
-        pygame.draw.rect(tela, VERMELHO, sair_rect)
+
+        # botão Sair
+        sair_rect = pygame.Rect(490, 622, 200, 50) # (1º Largura, 2º Altura)
+
+        # cria uma surface com transparência
+        sair_surface = pygame.Surface((sair_rect.width, sair_rect.height), pygame.SRCALPHA)
+
+        # pinta a surface (último valor é alpha → 0=transparente, 255=opaco)
+        sair_surface.fill((0, 255, 0, 0))  # semi-transparente
+
+        # desenha na tela
+        tela.blit(sair_surface, sair_rect.topleft)
+
+        # escreve o texto por cima
         desenhar_texto(tela, "Sair", fonte_opcoes, AZUL, sair_rect.centerx, sair_rect.y + 10)
 
         if sair_rect.collidepoint(mouse) and clique[0]:
